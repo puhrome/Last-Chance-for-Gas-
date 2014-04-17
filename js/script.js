25dbb8c509e526f231bc2e0fdf3e945ab7396861
puhrome <phirom.yim@fullsail.edu>
2014-04-16 20:22:04 -0400

    Made corrections and additions
    Updated correct formula and user input for prompts. Added comments on
    comment line.

--- js/script.js
+++ js/script.js
@@ -2,23 +2,23 @@
  * Last Chance for Gas! Created by Phirom Yim on 4/14/14.
  */
 //
-//gas mileage is var mpg
-var mpg = 35;
-//gas percentage is var gas
-var gas = 25;
-//tank capacity is var capacity
-var capacity = 15;
 
-//if gas is more than 40 percent which is 210 miles then no gas station stop needed
-//if gas is less than 40 percent then stop at gas station
-
-if(capacity * mpg < gas){
-//if tank capacity times gas mileage is less than gas percent
+var mpg = 35;//declared and defined miles per gallon
+var capacity = 15;//declared and defined tank capacity
+//miles per gallon times capacity gives mileage of a full tank
+var fullTank = mpg * capacity;
+//200 divided by full tank multiply by 100 gives the percentage of min gas required to make it
+var percentage = 200/fullTank * 100;
+//user input of gas percentage has to be greater than variable percentage
+var gasPercentage = Number (prompt("Enter Your Gas Percentage"));
+gasPercentage > percentage;
+//code performed if true
+if(gasPercentage > percentage){
+    //if tank capacity times gas mileage is less than gas percent
     console.log("You can make it without stopping for gas!");
 
 }else{
-//if greater than or equal to then get gas
-    console.log("You only have");
-    console.log(mpg * capacity /gas);
-    console.log("gallons of gas in your tank, better get gas while you can!");
+    //code performed if statement is false
+    console.log("You only have" + " " + gasPercentage + " " + "gallons of gas in your tank, better get gas while you can!");
+
 }
